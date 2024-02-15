1c0d87b159f6662f80f955fc27a0c2ec7129f1ec
BihanMadhusankha <ellawalagemadhusankha02@gmail.com>
2024-02-15 04:45:37 -0800

    start array function

--- icetpractice2/js/script.js
+++ icetpractice2/js/script.js
@@ -24,8 +24,30 @@ const Bihan={
     firstName : 'Bihan',
     lastName : 'Madhusankha',
     age : 22,
-    city : 'Kandy'
+    city : 'Kandy',
+    fullName : function(){
+        return this.firstName + ' ' + this.lastName
+    },
+
+    driving_licence:function(){
+        if(this.age>=18){
+            return 'Yes, '+this.firstName+' is '+this.age+' years old. so he can drive';
+        }
+        else{
+            return 'No , '+this.firstName+' is '+this.age+' years old. so he can not drive';
+        }
+
+    }
 }
+
+console.log(Bihan.fullName());
+
+console.log(Bihan.driving_licence());
+
+Bihan.age = 12;
+
+console.log(Bihan.driving_licence());
+
 console.log(Bihan.firstName);
 
 console.log();
@@ -35,3 +57,35 @@ let keys =['firstName','lastName','age','city'];
 keys.forEach(e =>{
     console.log(`${e}: ${Bihan[e]}`);
 })
+
+class person{
+    constructor(firstName,lastName,age,city){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.city = city;
+
+    }
+}
+
+person.prototype.fullName = function(){
+    return this.firstName + ' ' + this.lastName
+}
+
+let Hasini = new person('Hasini','Chamodi',23,'Meegoda')
+
+console.log(Hasini.fullName());
+console.log();
+
+let people = new Array();
+
+
+people.push('Bihan','Madhusankha','Hasini','Chamodi','Kasun');
+
+console.log(people);
+
+person.prototype.frends = function(){
+    return this.firstName + "'s frend are "+ people+".";
+}
+
+console.log(Hasini.frends());
